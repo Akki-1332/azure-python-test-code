7b6ffa955562b226d72b0d0e0c3557f96453e92f
Akash <choudharyaakash316@gmail.com>
2021-05-28 18:04:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Akki From Azure!  <b>hello !!</b>  <img src='https://myimagest.blob.core.windows.net/myclwimage/vimal.jpg'  width='150' height='200' />"
+    return "Hello Akki From Azure!  <b>hello !!"
